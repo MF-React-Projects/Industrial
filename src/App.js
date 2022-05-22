23b8e3532078995b46367d61da6a4e7212745057
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-22 13:46:02 +0600

    login, register component create

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import {Route, Routes} from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
             </Routes>
         </>
     );
