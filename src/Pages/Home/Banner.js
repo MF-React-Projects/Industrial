f02da64f48da2c2b485251509170542e094e9b17
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-23 21:22:30 +0600

    banner component

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import banner from '../../assets/img/banner.jpg';
+import {Link} from "react-router-dom";
+import {Container, Row, Col} from "react-bootstrap";
 
 const Banner = () => {
     return (
-        <div>
-            <h3>Banner</h3>
+        <div className="banner-section" style={{backgroundImage: `url(${banner})`}}>
+            <Container>
+                <Row>
+                    <Col lg='8'>
+                        <div className="banner-content">
+                            <h1 className=""><span>Best Manufecturer</span> in the world</h1>
+                            <h4 className="">Supplying the world with high quality chemical products.</h4>
+                            <Link to='/manage-inventories' className='btn-default-2'>Buy Now</Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
