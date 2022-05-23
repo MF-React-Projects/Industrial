49a06d630f762c55f5e75dd81216d8c606b6531f
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-24 01:44:04 +0600

    footer component add and design

--- src/Pages/Common/Footer.js
+++ src/Pages/Common/Footer.js
@@ -1,10 +1,103 @@
 import React from 'react';
+import logo from '../../../src/logo-white.png';
+import {Link} from "react-router-dom";
+import {Col, Container, Row} from "react-bootstrap";
+import {FaMapMarkerAlt} from "@react-icons/all-files/fa/FaMapMarkerAlt";
+import {FaPhone} from "@react-icons/all-files/fa/FaPhone";
+import {FaEnvelope} from "@react-icons/all-files/fa/FaEnvelope";
+import {useAuthState} from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import {signOut} from "firebase/auth";
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+
+    const [user] = useAuthState(auth);
+    const handleSignOut = () => {
+        signOut(auth);
+    };
     return (
-        <div>
-            <h2>Footer</h2>
-        </div>
+        <footer className='footer'>
+            <div className="footer-top">
+                <Container>
+                    <Row>
+                        <Col md={12} lg={4}>
+                            <div className="footer-about">
+                                <img src={logo} alt="logo" className="footer-logo mb-4"/>
+                                <p>
+                                    I am a Photographer from new york, i love to capture the moments and share them with
+                                    you. I travel the world and capture the moments that i love. Wildlife, nature,
+                                    architecture, food, and people are my favorite subjects.
+                                </p>
+                            </div>
+                        </Col>
+                        <Col md={6} lg={{span: 3, offset: 2}}>
+                            <div className="footer-contact">
+                                <h3 className='widget-title'>Contact Me</h3>
+                                <p>
+                                    <FaMapMarkerAlt className="me-2"/>
+                                    <span>Brooklyn, New York, USA</span>
+                                </p>
+                                <p>
+                                    <FaPhone className='me-2'/>
+                                    <span>+1 (212) 555-5555</span>
+                                </p>
+                                <p>
+                                    <FaEnvelope className={'me-2'}/>
+                                    <span>
+                                    admin@demo.com
+                                </span>
+                                </p>
+                            </div>
+                        </Col>
+                        <Col md={6} lg={3} >
+                            <div className="footer-links">
+                                <h3 className='widget-title'>Quick Links</h3>
+                                <ul>
+                                    <li>
+                                        <Link to="/">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/about">About</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/checkout">Checkout</Link>
+                                    </li>
+                                    {
+                                        user ?
+                                            <li>
+                                                <button onClick={handleSignOut} className='btn btn-link p-0'>Logout</button>
+                                            </li>
+                                            :
+                                            <>
+                                                <li>
+                                                    <Link to="/login">Login</Link>
+                                                </li>
+                                            </>
+                                    }
+
+                                </ul>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div className="footer-copyright">
+                <Container>
+                    <Row>
+                        <Col md={6} className='mb-2 mb-md-0'>
+                            <p>Copyright © {year} All rights reserved.</p>
+                        </Col>
+                        <Col md={6} className="footer-right">
+                            <p>
+                                Design & Developed by <strong className='p_color'>Mehedi Foysal</strong>
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </footer>
     );
 };
 
