b2d5a9c8ff46da0ec7ad3121854d7ba889ee44ac
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-24 03:06:22 +0600

    blog component markup

--- src/Pages/Home/Blogs.js
+++ src/Pages/Home/Blogs.js
@@ -2,16 +2,23 @@ import React from 'react';
 import {Container, Row} from "react-bootstrap";
 import SectionHeader from "../Common/SectionHeader";
 import {useNavigate} from "react-router-dom";
+import {useQuery} from "react-query";
+import Blog from "./Blog";
+import Loading from "../Common/Loading";
 
 const Blogs = () => {
     const navigate = useNavigate();
+    const {data: blogs, isLoading} = useQuery('blogs', () => fetch('http://localhost:5000/blogs?limit=3').then(res => res.json()));
+    console.log(blogs)
+    if(isLoading) return <Loading/>
+
     return (
         <section id="blog" className="home-blog-section">
             <Container>
                 <SectionHeader badge={'News Feeds'} title={'Latest News'}/>
                 <Row className='mb-5'>
                     {
-                        blogs.map(blog => <Blog key={blog.id} blog={blog}/>)
+                        blogs.map(blog => <Blog key={blog._id} blog={blog}/>)
                     }
                 </Row>
                 <div className="text-center mt-4">
