e329de7255109f64571f43ad4dca6afe87357870
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-24 10:53:26 +0600

    requireAuth add

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Login/Register";
 import Purchase from "./Pages/Purchase/Purchase";
+import RequireAuth from "./Pages/Login/RequireAuth";
 
 function App() {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/" element={
-                    <Purchase/>
+                <Route path="/product/:id" element={
+                    <RequireAuth><Purchase/></RequireAuth>
                 }/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import Header from "../Common/Header";
+import Footer from "../Common/Footer";
+import {Col, Container, Row} from "react-bootstrap";
 
 const Purchase = () => {
     const {id} = useParams();
     return (
-        <div>
-            {id}
-        </div>
+        <>
+            <Header/>
+            <div className='product-details'>
+                <Container>
+                    <Row>
+                        <Col lg={3}>
+                            <div className="product-single-thumb">
+                                <img src={image} alt="product-thumb" className='img-fluid'/>
+                            </div>
+                        </Col>
+                        <Col lg={9}>
+                            <div className="product-single-content">
+                                <h2>{name}</h2>
+                                <ul className='product-infos list-unstyled ps-0 ms-0 mb-3 d-flex align-items-center justify-content-between'>
+                                    <li><b>Price:</b> ${price}</li>
+                                    <li><b>Quantity:</b> {quantity}</li>
+                                    <li><b>Supplier:</b> {supplier}</li>
+                                </ul>
+                                <p>{description}</p>
+                                <div className="d-flex align-items-center mt-4">
+                                    <Button className='btn-default btn-secondary btnSm me-3' onClick={handleDeliver}>Delivered</Button>
+                                    <Link to='/manage-inventories' className='btn-default btnSm'>Manage Inventories</Link>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <Footer/>
+        </>
     );
 };
 
