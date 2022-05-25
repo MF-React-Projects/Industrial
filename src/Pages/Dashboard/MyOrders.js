06facf3f67b79198730a88392eb5ac4ce5bfa8b9
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-25 23:22:42 +0600

    load order data

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import {Table} from "react-bootstrap";
+import {useQuery} from "react-query";
+import {useAuthState} from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Common/Loading";
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+
+    const {data:orders, isLoading} = useQuery('orders', async () => {
+        const response = await fetch(`http://localhost:5000/orders/${user?.email}`);
+        return await response.json();
+    });
+
+    if(isLoading) return <Loading/>;
+
+    const handleCancel = () =>{
+
+    }
+
     return (
         <div>
             <h2 className='text-center p_color'>My Orders</h2>
@@ -17,6 +34,20 @@ const MyOrders = () => {
                 </tr>
                 </thead>
                 <tbody>
+                {
+                    orders.map((order, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{order.name}</td>
+                            <td>{order.email}</td>
+                            <td>{order.phone}</td>
+                            <td>{order.qty}</td>
+                            <td>
+                                <button className='btn btn-primary btn-sm' onClick={handleCancel}>Cancel</button>
+                            </td>
+                        </tr>
+                    ))
+                }
                 <tr>
                     <td>1</td>
                     <td>Mark</td>
